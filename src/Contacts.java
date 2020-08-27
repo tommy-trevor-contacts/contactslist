15555d479201f340af0fff4918aa384f66e50595
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-27 11:23:08 -0500

    quick update

--- src/Contacts.java
+++ src/Contacts.java
@@ -1,2 +1,29 @@
+import java.util.List;
 public class Contacts {
+    protected String name;
+    protected String phonenumber;
+//    protected List<String>
+
+    // CLASS CONSTRUCTOR
+    public void Contact(String name, String phonenumber) {
+        this.name = name;
+        this.phonenumber = phonenumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
 }
--- src/ContactsApp.java
+++ src/ContactsApp.java
@@ -1,3 +1,36 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContactsApp {
+    static String contactsDirectory = "db";
+    static String contactsData = "contacts.txt";
+    //Pathway to the contacts text file
+//    Path contact = Paths.get("./src/db", "contacts.txt");
+    static Path contactsFilePath = Paths.get("./src/" + contactsDirectory + "/" + contactsData);
+    //declares contacts array list
+//    static List<String> contacts;
+    static List<String> contactsList = new ArrayList<>();
+    //read content of contacts & add into contacts list array
+    if (Files.exists(contactsFilePath)) {
+        try {
+            contactsList = Files.readAllLines(contact);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void contactInterface() {
+
+
+        switch (userInput) {
+            case 1:
+
+
+        }
+
+    }
 
 }
