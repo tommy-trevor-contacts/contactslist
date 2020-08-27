65ea29b06c9f5bea295d3a3981d8f3b7681d9dd3
Trevor Arnold <arnold.j.trevor@gmail.com>
2020-08-27 15:23:30 -0500

    tommy im tired

--- src/ContactsApp.java
+++ src/ContactsApp.java
@@ -15,9 +15,8 @@ public class ContactsApp {
     //declares contacts array list
     List<Contact> contact;
     static List<String> contactsList = new ArrayList<>();
-//    static Contact contact;
+    //    static Contact contact;
     static Scanner scanner = new Scanner(System.in);
-
 
 
     // ======= METHODS =======
@@ -35,20 +34,36 @@ public class ContactsApp {
     }
 
     // Add Contact Method
-//    public static void addContact () {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Please Enter New Contact");
-//        String addName = scanner.next();
-//
-//    }
-
     public static void addContact(String name, String phone) throws IOException {
-
         Contact contact = new Contact(name, phone);
         contactsList.add(contact.getName() + " | " + contact.getPhoneNumber());
-
         Files.write(contactsFilePath, contactsList, StandardOpenOption.TRUNCATE_EXISTING);
     }
+
+    // Print Contacts
+    public static void printContacts(List<String> list) {
+        System.out.println("\nName | Phone number");
+        System.out.println("---------------");
+        for (String contact : list) {
+            System.out.println(contact);
+        }
+    }
+
+    // View All Method
+    public static void viewAll() throws IOException {
+        contactsList = Files.readAllLines(contactsFilePath);
+        printContacts(contactsList);
+    }
+
+    public static void addContact() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please Enter New Contact");
+        String addName = scanner.nextLine();
+        System.out.println("Please enter the contact number");
+        String addNumber = scanner.next();
+        System.out.println("The contact that will be added: " + addName + " | " + addNumber);
+    }
+}
 
 //    public void contactInterface() {
 //        switch (userInput) {
